1dce04071e3aea578e8405ab81040f9ebea4ba6a
MonikaKumarasamy <136447648+MonikaKumarasamy@users.noreply.github.com>
2023-06-21 15:58:02 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -34,6 +34,6 @@ TEST_CASE("Print message 4") {
   sendToEmail(TOO_HIGH);
 }
 TEST_CASE("Print message 5") {
-  checkAndAlert(TO_CONTROLLER, coolingType, 12);
+  checkAndAlert(TO_CONTROLLER, BatteryCharacter::coolingType, 12);
 }
 
