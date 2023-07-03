ebe76040ec60aa7078f5d7c5f09e10ed8e6746ec
MonikaKumarasamy <136447648+MonikaKumarasamy@users.noreply.github.com>
2023-07-03 14:33:49 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,4 +1,10 @@
 #pragma once
+public:
+typedef enum {
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+} CoolingType;
 typedef enum {
   NORMAL,
   TOO_LOW,
@@ -12,19 +18,14 @@ typedef enum {
   TO_CONTROLLER,
   TO_EMAIL
 } AlertTarget;
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
-public:
+
 typedef struct {
   CoolingType coolingType;
   char brand[48];
 } BatteryCharacter;
-typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
-} CoolingType;
 
 void checkAndAlert(
   AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+void sendToController(BreachType breachType);
+void sendToEmail(BreachType breachType);
 
